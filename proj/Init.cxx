17ef9bb96326f8f2289168dc4b3e7ae059d8db9c
yzyTUD <yzhongyuanfff@gmail.com>
2020-11-16 13:20:12 +0100

    add them back

---
+++ proj/Init.cxx
@@ -0,0 +1,8 @@
+﻿// This source code is property of the Computer Graphics and Visualization 
+// chair of the TU Dresden. Do not distribute! 
+// Copyright (C) CGV TU Dresden - All Rights Reserved
+//
+// The main file of the plugin. It defines a global register variable
+// "global_initializer" that is initialized when the plugin is loaded.
+
+
